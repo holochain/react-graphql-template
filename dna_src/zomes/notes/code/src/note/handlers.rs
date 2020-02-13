8cd18fb2fc0f807cfaff25ba14ec1e27dc040e95
Philip Beadle <philip.beadle@live.com.au>
2020-02-14 07:10:19 +1100

    Weird didnt pick these files before

--- dna_src/zomes/notes/code/src/note/handlers.rs
+++ dna_src/zomes/notes/code/src/note/handlers.rs
@@ -1,31 +1,39 @@
-// use uuid::Uuid;
 use hdk::error::ZomeApiResult;
 use hdk::holochain_core_types::entry::Entry;
-use hdk::holochain_persistence_api::cas::content::Address;
+use hdk::holochain_persistence_api::cas::content::{
+    Address,
+    AddressableContent
+};
 use hdk::prelude::LinkMatch;
 use holochain_anchors::anchor;
+use std::convert::TryFrom;
 use crate::note::NoteSpec;
+use crate::note::NoteEntry;
 use crate::note::Note;
 use crate::note;
 
-pub fn create_note(note_spec: NoteSpec) -> ZomeApiResult<Address> {
+pub fn create_note(note_spec: NoteSpec) -> ZomeApiResult<Note> {
     hdk::debug(format!("create_note: {:?}", note_spec)).ok();
-    let note_uuid = "a uuid".to_string(); //Uuid::new_v4(); Doesnt work in WASM need to ask David Meister
-    hdk::debug(format!("note_uuid: {:?}", note_uuid)).ok();
-    let note = note::Note::from_spec(&note_spec, &note_uuid.to_string());
-    hdk::debug(format!("Note Posted: {:?}", note)).ok();
-    let entry = Entry::App(note::NOTE_ENTRY_NAME.into(), note.into());
+    let id: Address = note_spec.created_at.to_string().into();
+    let note_entry = note::NoteEntry::from_spec(&note_spec, &id.to_string());
+    let entry = Entry::App(note::NOTE_ENTRY_NAME.into(), note_entry.clone().into());
     let address = hdk::commit_entry(&entry)?;
     hdk::link_entries(&anchor(note::NOTE_ANCHOR_TYPE.to_string(), note::NOTE_ANCHOR_TEXT.to_string())?, &address, note::NOTE_LINK_TYPE, "")?;
-    Ok(address)
+    let note = note::Note::from_entry(&note_entry, &address);
+    Ok(note)
 }
 
 pub fn get_note(address: Address) -> ZomeApiResult<Note> {
-    Ok(hdk::utils::get_as_type(address)?)
+    let note_entry = hdk::utils::get_as_type(address.clone())?;
+    let note = note::Note::from_entry(&note_entry, &address);
+    Ok(note)
 }
 
-pub fn update_note(note: Note, address: Address) -> ZomeApiResult<Address> {
-    Ok(hdk::update_entry(Entry::App(note::NOTE_ENTRY_NAME.into(), note.clone().into()), &address)?)
+pub fn update_note(note: Note) -> ZomeApiResult<Note> {
+    let note_entry = note::NoteEntry::from_note(&note);
+    let updated_address = hdk::update_entry(Entry::App(note::NOTE_ENTRY_NAME.into(), note_entry.clone().into()), &note.address)?;
+    let note = note::Note::from_entry(&note_entry, &updated_address);
+    Ok(note)
 }
 
 pub fn remove_note(address: Address) -> ZomeApiResult<Address> {
@@ -33,5 +41,20 @@ pub fn remove_note(address: Address) -> ZomeApiResult<Address> {
 }
 
 pub fn list_notes() -> ZomeApiResult<Vec<Note>> {
-    hdk::utils::get_links_and_load_type(&anchor(note::NOTE_ANCHOR_TYPE.to_string(), note::NOTE_ANCHOR_TEXT.to_string())?, LinkMatch::Exactly(note::NOTE_LINK_TYPE), LinkMatch::Any)
+    hdk::get_links_and_load(&anchor(note::NOTE_ANCHOR_TYPE.to_string(),
+    note::NOTE_ANCHOR_TEXT.to_string())?, LinkMatch::Exactly(note::NOTE_LINK_TYPE), LinkMatch::Any)
+        .map(|note_list|{
+            note_list.into_iter()
+                .filter_map(Result::ok)
+                .flat_map(|entry| {
+                    let address = entry.address();
+                    if let Entry::App(_, value) = entry {
+                        NoteEntry::try_from(value)
+                            .ok()
+                            .map(|note_entry| note::Note::from_entry(&note_entry, &address))
+                    } else {
+                        None
+                    }
+                }).collect()
+        })
 }
