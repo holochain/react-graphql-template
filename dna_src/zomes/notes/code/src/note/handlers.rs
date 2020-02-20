4a6fdfa61d6738b8cc3ea64ae7eda56584b3deaa
Philip Beadle <philip.beadle@live.com.au>
2020-02-20 12:11:02 +1100

    validation logic uses source in old and new entry now, also tidied up the code

--- dna_src/zomes/notes/code/src/note/handlers.rs
+++ dna_src/zomes/notes/code/src/note/handlers.rs
@@ -2,20 +2,12 @@ use hdk::{
     error::ZomeApiResult,
     holochain_core_types::{
         entry::Entry,
-        time::Timeout,
-        time::Iso8601
     },
     holochain_persistence_api::cas::content::{
         Address,
         AddressableContent
     },
-    prelude::{
-        LinkMatch,
-        GetEntryOptions,
-        GetEntryResult,
-        GetEntryResultType::Single,
-        StatusRequestKind
-    }
+    prelude::*,
 };
 use holochain_anchors::anchor;
 use crate::note::{
@@ -31,39 +23,16 @@ fn notes_anchor() -> ZomeApiResult<Address> {
     anchor(NOTES_ANCHOR_TYPE.to_string(), NOTES_ANCHOR_TEXT.to_string())
 }
 
-fn get_initial_entry(address: Address) -> ZomeApiResult<GetEntryResult> {
-    let options = GetEntryOptions{status_request: StatusRequestKind::Initial, entry: false, headers: true, timeout: Timeout::new(10000)};
-    hdk::get_entry_result(&address, options)
-}
-
-fn timestamp(entry_result: GetEntryResult) -> Iso8601 {
-    match entry_result.result {
-        Single(entry) => {
-            hdk::debug(format!("single_entry_timestamp: {:?}", entry.headers[0].timestamp())).ok();
-            entry.headers[0].timestamp().clone()
-        },
-        _ => {
-            hdk::debug(format!("not_single_entry")).ok();
-            Iso8601::new(0,0)
-        }
-    }
-}
-
 pub fn create_note(note_entry: NoteEntry) -> ZomeApiResult<Note> {
-    hdk::debug(format!("create_note: {:?}", note_entry)).ok();
     let entry = Entry::App(NOTE_ENTRY_NAME.into(), note_entry.clone().into());
     let address = hdk::commit_entry(&entry)?;
-    let entry_result = get_initial_entry(address.clone())?;
     hdk::link_entries(&notes_anchor()?, &address, NOTE_LINK_TYPE, "")?;
-    let note = Note::from_result(&address, &timestamp(entry_result), &note_entry.title, &note_entry.content);
-    Ok(note)
+    Note::new(address, note_entry)
 }
 
 pub fn get_note(id: Address) -> ZomeApiResult<Note> {
-    let entry_result = get_initial_entry(id.clone())?;
     let note: NoteEntry = hdk::utils::get_as_type(id.clone())?;
-    let note = Note::from_result(&id, &timestamp(entry_result), &note.title, &note.content);
-    Ok(note)
+    Note::new(id, note)
 }
 
 pub fn update_note(id: Address, note_input: NoteEntry) -> ZomeApiResult<Note> {
@@ -72,7 +41,7 @@ pub fn update_note(id: Address, note_input: NoteEntry) -> ZomeApiResult<Note> {
         Some(entry) => entry.address()
     };
     hdk::update_entry(Entry::App(NOTE_ENTRY_NAME.into(), note_input.clone().into()), &address)?;
-    get_note(id.clone())
+    Note::new(id, note_input)
 }
 
 pub fn remove_note(id: Address) -> ZomeApiResult<Address> {
